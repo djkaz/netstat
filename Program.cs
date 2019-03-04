d0f1c2c5ea4c4c2201852168f8f4487d5e7042e9
djkaz <stefiaxe@yahoo.com>
2019-03-04 16:17:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] endPoints = ipProperties.GetActiveTcpListeners();
+
+            TcpConnectionInformation[] tcpConnections = ipProperties.GetActiveTcpConnections();
+            foreach (TcpConnectionInformation info in tcpConnections) {
+
+                Console.WriteLine("Local : " + info.LocalEndPoint.Address.ToString()
+                + ":" + info.LocalEndPoint.Port.ToString()
+                + "\nRemote : " + info.RemoteEndPoint.Address.ToString()
+                + ":" + info.RemoteEndPoint.Port.ToString()
+                + "\nState : " + info.State.ToString() + "\n\n");
+            }
+
+            
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
